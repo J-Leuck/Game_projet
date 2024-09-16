d8fc4cffc7a788f105e386c5ca6dd859553daa35
Zawelon <zawelongoumou@gmail.com>
2024-09-16 18:30:03 +0200

    Create class Jeu

--- My_Class_file.py
+++ My_Class_file.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python
 
+import random
+
+class Jeu():
+    def __init__(self, Pc_val, user_input):
+        self.Pc_val = Pc_val
+        self.user_input =user_input
+
+    def random_fonction(self) -> int:
+        
+        return self.Pc_val
+
+
+
+    def logique_du_jeu(self, int a):
+
+
+    def guide_intervalle_choix(self):
+
+
